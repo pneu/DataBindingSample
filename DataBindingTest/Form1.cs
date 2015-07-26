d0fa1e82aab455bfd650b0cccf948d819cab11a3
pneu <pneumaster@gmail.com>
2015-07-26 21:52:12 +0900

    DataBinding Sample

---
+++ DataBindingTest/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBindingTest
+{
+    public partial class Form1 : Form
+    {
+        MyData DataInstance;
+
+        public Form1() {
+            InitializeComponent();
+            DataInstance = new MyData() { Number = 100 };
+            this.bindingSource1.DataSource = DataInstance;
+            this.bindingSource1.DataSourceChanged += bindingSource1_DataSourceChanged;
+        }
+
+        void bindingSource1_DataSourceChanged(object sender, EventArgs e) {
+            MessageBox.Show("DataSourceChanged");
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            this.DataInstance = new MyData() { Number = 200 };
+            this.bindingSource1.DataSource = DataInstance;
+            this.bindingSource1.ResetBindings(false);
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            DataInstance.Number *= 5;
+            this.bindingSource1.ResetBindings(false);
+        }
+    }
+}
